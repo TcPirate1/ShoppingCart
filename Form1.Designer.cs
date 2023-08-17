940e15456e953ff87ccbccf46ce6389024a7d9f9
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2023-08-18 11:27:17 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ShoppingCart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductsListBox = new System.Windows.Forms.ListBox();
+            this.ProductListLabel = new System.Windows.Forms.Label();
+            this.ShoppingCartLabel = new System.Windows.Forms.Label();
+            this.TotalLabel = new System.Windows.Forms.Label();
+            this.ShoppingCartListBox = new System.Windows.Forms.ListBox();
+            this.CheckOutButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProductsListBox
+            // 
+            this.ProductsListBox.FormattingEnabled = true;
+            this.ProductsListBox.Location = new System.Drawing.Point(150, 84);
+            this.ProductsListBox.Name = "ProductsListBox";
+            this.ProductsListBox.Size = new System.Drawing.Size(150, 160);
+            this.ProductsListBox.TabIndex = 0;
+            // 
+            // ProductListLabel
+            // 
+            this.ProductListLabel.AutoSize = true;
+            this.ProductListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProductListLabel.Location = new System.Drawing.Point(145, 53);
+            this.ProductListLabel.Name = "ProductListLabel";
+            this.ProductListLabel.Size = new System.Drawing.Size(126, 25);
+            this.ProductListLabel.TabIndex = 1;
+            this.ProductListLabel.Text = "Product List";
+            // 
+            // ShoppingCartLabel
+            // 
+            this.ShoppingCartLabel.AutoSize = true;
+            this.ShoppingCartLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShoppingCartLabel.Location = new System.Drawing.Point(481, 53);
+            this.ShoppingCartLabel.Name = "ShoppingCartLabel";
+            this.ShoppingCartLabel.Size = new System.Drawing.Size(149, 25);
+            this.ShoppingCartLabel.TabIndex = 2;
+            this.ShoppingCartLabel.Text = "Shopping Cart";
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.AutoSize = true;
+            this.TotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalLabel.Location = new System.Drawing.Point(475, 268);
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(98, 25);
+            this.TotalLabel.TabIndex = 3;
+            this.TotalLabel.Text = "Total ($):";
+            // 
+            // ShoppingCartListBox
+            // 
+            this.ShoppingCartListBox.FormattingEnabled = true;
+            this.ShoppingCartListBox.Location = new System.Drawing.Point(480, 84);
+            this.ShoppingCartListBox.Name = "ShoppingCartListBox";
+            this.ShoppingCartListBox.Size = new System.Drawing.Size(150, 160);
+            this.ShoppingCartListBox.TabIndex = 4;
+            // 
+            // CheckOutButton
+            // 
+            this.CheckOutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckOutButton.Location = new System.Drawing.Point(480, 346);
+            this.CheckOutButton.Name = "CheckOutButton";
+            this.CheckOutButton.Size = new System.Drawing.Size(121, 57);
+            this.CheckOutButton.TabIndex = 5;
+            this.CheckOutButton.Text = "Check Out";
+            this.CheckOutButton.UseVisualStyleBackColor = true;
+            this.CheckOutButton.Click += new System.EventHandler(this.CheckOutButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(307, 84);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(53, 31);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.CheckOutButton);
+            this.Controls.Add(this.ShoppingCartListBox);
+            this.Controls.Add(this.TotalLabel);
+            this.Controls.Add(this.ShoppingCartLabel);
+            this.Controls.Add(this.ProductListLabel);
+            this.Controls.Add(this.ProductsListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ProductsListBox;
+        private System.Windows.Forms.Label ProductListLabel;
+        private System.Windows.Forms.Label ShoppingCartLabel;
+        private System.Windows.Forms.Label TotalLabel;
+        private System.Windows.Forms.ListBox ShoppingCartListBox;
+        private System.Windows.Forms.Button CheckOutButton;
+        private System.Windows.Forms.Button AddButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingCart
+{
+    public partial class Form1 : Form
+    {
+        readonly ArrayList cartItems = new ArrayList();
+        readonly List<Product> products = new List<Product>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeProducts();
+        }
+
+        private void InitializeProducts()
+        {
+            // Products that are avaliable in the list
+            products.Add(new Product("Chicken Drumsticks", 5.00m));
+            products.Add(new Product("Chicken Legs", 11.49m));
+            products.Add(new Product("Beef Rump Steak", 22.99m));
+            products.Add(new Product("Lamb Leg Roast", 13.69m));
+            products.Add(new Product("Whole Snapper", 21.99m));
+            products.Add(new Product("Salmon Fillets", 44.99m));
+            products.Add(new Product("Portobello Mushroom", 18.79m));
+            products.Add(new Product("Red Chilli", 1.89m));
+            products.Add(new Product("Unwashed Potatoes", 2.99m));
+
+            ProductsListBox.DataSource = products;
+            ProductsListBox.DisplayMember = "Name";
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = ProductsListBox.SelectedIndex;
+
+            if (selectedIndex >= 0)
+            {
+                Product selectedProduct = products[selectedIndex];
+                cartItems.Add(selectedProduct);
+                ShoppingCartListBox.Items.Add(selectedProduct.Name);
+            }
+        }
+
+        private void CheckOutButton_Click(object sender, EventArgs e)
+        {
+            {
+                decimal total = Product.TotalCalc(cartItems.OfType<Product>());
+                string message = "Items in your cart:\n\n";
+
+                foreach (Product product in cartItems)
+                {
+                    message += $"{product.Name} - ${product.Price}\n";
+                }
+
+                message += $"\nTotal: ${total}";
+
+                MessageBox.Show(message, "Total");
+            }
+        }
+    }
+}
