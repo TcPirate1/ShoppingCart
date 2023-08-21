0d74858992656833bcb301a7468f95228caebea4
TC_win_laptop <88120195+TcPirate1@users.noreply.github.com>
2023-08-21 15:04:08 +1200

    Removed Total label

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.ProductsListBox = new System.Windows.Forms.ListBox();
             this.ProductListLabel = new System.Windows.Forms.Label();
             this.ShoppingCartLabel = new System.Windows.Forms.Label();
-            this.TotalLabel = new System.Windows.Forms.Label();
             this.ShoppingCartListBox = new System.Windows.Forms.ListBox();
             this.CheckOutButton = new System.Windows.Forms.Button();
             this.AddButton = new System.Windows.Forms.Button();
@@ -40,18 +39,21 @@
             // ProductsListBox
             // 
             this.ProductsListBox.FormattingEnabled = true;
-            this.ProductsListBox.Location = new System.Drawing.Point(150, 84);
+            this.ProductsListBox.ItemHeight = 16;
+            this.ProductsListBox.Location = new System.Drawing.Point(200, 103);
+            this.ProductsListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ProductsListBox.Name = "ProductsListBox";
-            this.ProductsListBox.Size = new System.Drawing.Size(150, 160);
+            this.ProductsListBox.Size = new System.Drawing.Size(199, 196);
             this.ProductsListBox.TabIndex = 0;
             // 
             // ProductListLabel
             // 
             this.ProductListLabel.AutoSize = true;
             this.ProductListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProductListLabel.Location = new System.Drawing.Point(145, 53);
+            this.ProductListLabel.Location = new System.Drawing.Point(193, 65);
+            this.ProductListLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ProductListLabel.Name = "ProductListLabel";
-            this.ProductListLabel.Size = new System.Drawing.Size(126, 25);
+            this.ProductListLabel.Size = new System.Drawing.Size(158, 31);
             this.ProductListLabel.TabIndex = 1;
             this.ProductListLabel.Text = "Product List";
             // 
@@ -59,36 +61,30 @@
             // 
             this.ShoppingCartLabel.AutoSize = true;
             this.ShoppingCartLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ShoppingCartLabel.Location = new System.Drawing.Point(481, 53);
+            this.ShoppingCartLabel.Location = new System.Drawing.Point(641, 65);
+            this.ShoppingCartLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ShoppingCartLabel.Name = "ShoppingCartLabel";
-            this.ShoppingCartLabel.Size = new System.Drawing.Size(149, 25);
+            this.ShoppingCartLabel.Size = new System.Drawing.Size(187, 31);
             this.ShoppingCartLabel.TabIndex = 2;
             this.ShoppingCartLabel.Text = "Shopping Cart";
-            // 
-            // TotalLabel
-            // 
-            this.TotalLabel.AutoSize = true;
-            this.TotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TotalLabel.Location = new System.Drawing.Point(475, 268);
-            this.TotalLabel.Name = "TotalLabel";
-            this.TotalLabel.Size = new System.Drawing.Size(98, 25);
-            this.TotalLabel.TabIndex = 3;
-            this.TotalLabel.Text = "Total ($):";
             // 
             // ShoppingCartListBox
             // 
             this.ShoppingCartListBox.FormattingEnabled = true;
-            this.ShoppingCartListBox.Location = new System.Drawing.Point(480, 84);
+            this.ShoppingCartListBox.ItemHeight = 16;
+            this.ShoppingCartListBox.Location = new System.Drawing.Point(640, 103);
+            this.ShoppingCartListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ShoppingCartListBox.Name = "ShoppingCartListBox";
-            this.ShoppingCartListBox.Size = new System.Drawing.Size(150, 160);
+            this.ShoppingCartListBox.Size = new System.Drawing.Size(199, 196);
             this.ShoppingCartListBox.TabIndex = 4;
             // 
             // CheckOutButton
             // 
             this.CheckOutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CheckOutButton.Location = new System.Drawing.Point(480, 346);
+            this.CheckOutButton.Location = new System.Drawing.Point(640, 386);
+            this.CheckOutButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CheckOutButton.Name = "CheckOutButton";
-            this.CheckOutButton.Size = new System.Drawing.Size(121, 57);
+            this.CheckOutButton.Size = new System.Drawing.Size(161, 70);
             this.CheckOutButton.TabIndex = 5;
             this.CheckOutButton.Text = "Check Out";
             this.CheckOutButton.UseVisualStyleBackColor = true;
@@ -97,9 +93,10 @@
             // AddButton
             // 
             this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AddButton.Location = new System.Drawing.Point(307, 84);
+            this.AddButton.Location = new System.Drawing.Point(409, 103);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(53, 31);
+            this.AddButton.Size = new System.Drawing.Size(71, 38);
             this.AddButton.TabIndex = 6;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
@@ -107,16 +104,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.AddButton);
             this.Controls.Add(this.CheckOutButton);
             this.Controls.Add(this.ShoppingCartListBox);
-            this.Controls.Add(this.TotalLabel);
             this.Controls.Add(this.ShoppingCartLabel);
             this.Controls.Add(this.ProductListLabel);
             this.Controls.Add(this.ProductsListBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -129,7 +126,6 @@
         private System.Windows.Forms.ListBox ProductsListBox;
         private System.Windows.Forms.Label ProductListLabel;
         private System.Windows.Forms.Label ShoppingCartLabel;
-        private System.Windows.Forms.Label TotalLabel;
         private System.Windows.Forms.ListBox ShoppingCartListBox;
         private System.Windows.Forms.Button CheckOutButton;
         private System.Windows.Forms.Button AddButton;
